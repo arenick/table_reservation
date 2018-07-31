062c40e93cf397622e710d4ce9fc0ad6c9dbb3bf
Ashley Renick <renickmail@gmail.com>
2018-07-31 09:40:32 -0400

    the form toggling on and off

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function () {
     
+    // let currentTable = null;
+
+    // for (let i = 0; i <9; i++) {
+    //     $("#table").append(`<div>${(i+1)}</div>`);
+    // }
+    // $("#table giv").attr("class","table div.available");
+
+    // $("body").on("click", ".table div.available"), (e) => {
+    //     $(form).show();
+    //         currentTable = $(e.target);
+    // });
+
     let table;
 
     $(".table").on("click", (e) => {
@@ -18,8 +30,16 @@ $(document).ready(function () {
         $("form").hide();
     });
 
-    $(".save").click(function() {
-        $(table).addClass(".reserved")
-    })
+    $(".save").on("click", () => {
+        $(".table").addClass(".reserved")
+    });
+
+    // $("body").on("click", "form button:first", (e)  => {
+    //     $("form").hide();
+    //     //we need to add the element to remove the class form
+    //     currentTable
+    //     .removeClass("available");
+    //     .addClass("reserved");
+    // });
 
 });
